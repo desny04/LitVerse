6f23e4e83d28e3ecead4bfd93485e42ec4e8f62f
desny <trsadesny@gmail.com>
2026-03-06 16:04:35 +0530

    home page

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -16,7 +16,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='books/')
+    image = models.ImageField(upload_to='book/')
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
 
     def __str__(self):
