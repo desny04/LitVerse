8f8e9b7b0588e12e8dea66c9db6bf3976b7555d4
desny <trsadesny@gmail.com>
2026-03-09 17:52:36 +0530

    home page

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Book
 
 def home(request):
@@ -16,3 +17,30 @@ def home(request):
     }
 
     return render(request, "home.html", context)
+
+def home(request):
+
+    query = request.GET.get('q')
+    category = request.GET.get('category')
+
+    books = Book.objects.all()
+
+    # search filter
+    if query:
+        books = books.filter(
+            Q(title__icontains=query) |
+            Q(author__icontains=query) |
+            Q(category__icontains=query)
+        )
+
+    # category filter
+    if category and category != "all":
+        books = books.filter(category=category)
+
+    context = {
+        'books': books,
+        'query': query,
+        'category': category
+    }
+
+    return render(request, 'home.html', context)
